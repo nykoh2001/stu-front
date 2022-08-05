6aeed897240dbd7365d1bc7ade74acd3431b7893
nykoh2001 <nykoh2001@gmail.com>
2022-08-05 15:30:14 +0900

    feat: add etc to login branch

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import SignUp from "./pages/SignUp/SignUp";
+import Login from "./pages/Login/Login";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <SignUp />
+    <Login />
   </React.StrictMode>
 );
 
