e1f311334aa0f43064833c0a8156d6a0c2c5004c
nykoh2001 <nykoh2001@gmail.com>
2022-08-06 22:19:18 +0900

    feat: add main page

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import * as Styled from "./Styled";
+import Header from "component/Header/Header";
+
+const Main = (): JSX.Element => {
+  return (
+    <Styled.Container>
+      <Header />
+    </Styled.Container>
+  );
+};
+
+export default Main;
