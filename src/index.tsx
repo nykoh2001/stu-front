571bb46cfb10ada6613d49bc9a10a727387e81c3
nykoh2001 <nykoh2001@gmail.com>
2022-09-14 09:57:22 +0900

    chore: install package

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import App from "router/app";
 
+// const BASE_URL = process.env.REACT_APP_BASE_URL;
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
